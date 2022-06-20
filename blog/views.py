9456f25c1ae403635c9ad88d4697f5d8d9ddeb8f
allen0818 <chshman310222@gmail.com>
2022-06-20 19:36:23 +0800

    change blog api to use generic api

--- blog/views.py
+++ blog/views.py
@@ -1,53 +1,67 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
+
+# from rest_framework import mixins
+from rest_framework import generics
 
 from .models import Article
 from .serializers import ArticleSerializer
 
 # Create your views here.
-@api_view(['GET', 'POST'])
-def article_list(request, format=None):
-    """
-    List all articles, or create a new article
-    """
+class ArticleList(generics.ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
 
-    print('user', request.user) # debug
+    # 將request.user與author綁定。調用create方法時執行如下函數。
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
 
-    if request.method == 'GET':
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
 
-    elif request.method == 'POST':
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            # associate request.user with author
-            serializer.save(author=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+# @api_view(['GET', 'POST'])
+# def article_list(request, format=None):
+#     """
+#     List all articles, or create a new article
+#     """
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def article_detail(request, pk, format=None):
-    """
-    Retrieve，update or delete an article instance。
-    """
+#     print('user', request.user) # debug
 
-    try:
-        article = Article.objects.get(pk=pk)
-    except Article.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+#     if request.method == 'GET':
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles, many=True)
+#         return Response(serializer.data)
 
-    if request.method == 'GET':
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = ArticleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             # associate request.user with author
+#             serializer.save(author=request.user)
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    elif request.method == 'PUT':
-        serializer = ArticleSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def article_detail(request, pk, format=None):
+#     """
+#     Retrieve，update or delete an article instance。
+#     """
 
-    elif request.method == 'DELETE':
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)        
+#     try:
+#         article = Article.objects.get(pk=pk)
+#     except Article.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#         serializer = ArticleSerializer(article)
+#         return Response(serializer.data)
+
+#     elif request.method == 'PUT':
+#         serializer = ArticleSerializer(article, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     elif request.method == 'DELETE':
+#         article.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)        
